a524af58d9aaf2443a689eb08130cc8b0dbd2da3
Charlotte Fereday <cbrfereday@gmail.com>
2017-02-19 20:06:41 +0000

    Completes solution for first exercise

--- saying_hello_ex1.rb
+++ saying_hello_ex1.rb
@@ -1,1 +1,25 @@
-#Turning data into information that can be used to make decisions 
+#Turning data into information that can be used to make decisions
+
+#Problem: Create a program that prompts for your name & prints a greeting using your name
+#Example output:  What is your name? Brian // Hello, Brian, nice to meet you!
+
+#Constraint: keep the input, string concatenation & output separate
+
+#Nouns: inputs & outputs:
+# program
+#name
+#greeting
+
+#verbs: processes:
+#prompts
+#prints
+
+def saying_hello
+  puts "What's your name?"
+  name = gets.chomp
+  puts "Hello, " + name + ", nice to meet you!"
+end
+
+saying_hello
+
+#can create new variables in body, doesn't need a parameter
