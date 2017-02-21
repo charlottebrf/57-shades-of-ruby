b316ceb6b87bb6c33dc7b0b683c50b4cecff5081
Charlotte Fereday <cbrfereday@gmail.com>
2017-02-21 08:40:16 +0000

    Updates first attempt at program, with erros

---
+++ 05_simple_math.rb
@@ -0,0 +1,46 @@
+# Problem: write a program that prompts for two numbers. Print the sum, difference, prdouct, and quotient of those numbers.
+#
+# Example output:
+# What is the first number? 10
+# What is the second number? 5
+# 10 + 5 = 15
+# 10 - 5 = 5
+# 10 * 5 = 50
+# 10 / 5 = 2
+#
+# Constraints:
+# • Values coming from users will be strings. Ensure you convert these values to numbers before doing the maths.
+# • Keep the inputs & outputs separate from the numerical conversions & other processing.
+# • Generate a single output statement with line breaks in the appropriate spots.
+
+# Pseudocode:
+# prompt "What is the first number?"
+# first_number = gets.chomp.to_i
+# prompt "What is the second number?"
+# second_number = gets.chomp.to_i
+# first_number + second_number
+# first_number - second_number
+# first_number * second_number
+# first_number / second_number
+
+def simple_math
+  puts "What is the first number"
+  first_number = gets.to_i
+  # first_number = (gets.chomp).to_i
+  # first_number = Integer(gets.chomp)
+  puts "What is the second number?"
+  second_number = gets.to_i
+  p first_number + second_number
+  p first_number - second_number
+  p first_number * second_number
+  p first_number / second_number
+end
+
+simple_math
+
+# How do you make tests for gets.chomp cases?
+# RSpec.describe "simple math" do
+#   it "turns the inputted string to an integer" do
+#     expect(simple_math("five")).to eq (5)
+#   end
+# end
