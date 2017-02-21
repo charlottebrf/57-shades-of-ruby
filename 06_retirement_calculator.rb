4d920ad2ac5ae480197170d2797247dba1c6a822
Charlotte Fereday <cbrfereday@gmail.com>
2017-02-21 18:20:21 +0000

    Adds in challenge 6

---
+++ 06_retirement_calculator.rb
@@ -0,0 +1,11 @@
+#Problem: create a program that determines how many years you have left until retirement & the year you can retire. It should prompt you for your current age & the age you want to retire & display the output as shown in the example that follows.
+
+#Example output:
+What is your current age? 25
+At what age would you like to retire? 65
+You have 40 years left until you can retire.
+It's 2015, so you can retire in 2055.
+
+Constraints:
+-Again be sure to convert the input to numerical data before doing any math.
+-Don't hard-code the current year into your program. Get it from the system via your programming language.
