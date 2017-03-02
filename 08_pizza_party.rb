39a1c089a88af46208506ce4a5d2bba849b447d7
Charlotte Fereday <cbrfereday@gmail.com>
2017-03-02 07:48:32 +0000

    Updates first draft of pseudocode for programme

---
+++ 08_pizza_party.rb
@@ -0,0 +1,54 @@
+# Problem: Write a program to evenly describe pizzas.Prompt for the number of people, the number of pizzas, and the number of slices per pizza. Ensure that the number of pieces comes out even. Display the number of pieces of pizza each person should get. If there are leftovers. show the number of leftover pieces.
+#
+# Example Output:
+# How many people? 8
+# How many pizzas do you have? 2
+#
+# 8 people with 2 pizzas
+# Each person gets 2 pieces of pizza.
+# There are 0 leftover pieces.
+
+# pseudocode:
+# number of people
+# prompt "How many people?"
+# store number of people
+#
+# number of pizzas
+# prompt "How many pizzas do you have?"
+# store number of pizzas
+#
+# Store gets.chomp data for programme
+# def main()
+#   people = number_of_people()
+#   pizzas = number_of_pizzas()
+#   do_stuff(people, pizzas)
+# end
+#
+# check for digits(n)
+# if digits true
+# else false
+#
+# check for integer
+# if integer true
+# if float false
+#
+# confirm number of people & pizzas
+# puts "#{number_of_people} with #{number_of_pizzas}"
+#
+# calculates number of slices(number_of_people, number_of_pizzas)
+# number_of_people / number_of_pizzas
+# *do I use / or %?
+# *"Ensure that the number of pieces comes out even"- does this mean division or is it asking for the result to always be an even number?*
+#
+# rounds number of slices(number_of_slices)
+# calculates number of slices.to_i
+# *Double check how to ensure number of slices is rounded to a whole number*
+#
+# calculates number of left over slices
+# *calculates the remainder*
+#
+# displays the number of left over slices(remainder)
+# puts "There are #{remainder} leftover pieces."
+#
+# do_stuff main programme
+# calls methods in correct order to display
