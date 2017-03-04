dcc45e5d6678c3dba5f3650244001b6d46032a9e
Charlotte Fereday <cbrfereday@gmail.com>
2017-03-04 18:15:39 +0000

    Adds in charlotte factorial method

---
+++ charlotte.rb
@@ -0,0 +1,13 @@
+puts "Hi Charlotte"
+puts (1 + 2)
+
+
+def factorial(number)
+  if (number > 1)
+    number * factorial(number -1)/Users/charlotte/Development/ruby/57-shades-of-ruby
+  else
+    1
+  end
+end
+
+puts factorial(3)
