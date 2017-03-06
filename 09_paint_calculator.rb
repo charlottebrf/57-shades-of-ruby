1ff967f52caa2b4b67f89da57e42198ef7721357
Charlotte Fereday <cbrfereday@gmail.com>
2017-03-06 07:52:33 +0000

    Updates paint calculator exercise & pseudocode for solution

---
+++ 09_paint_calculator.rb
@@ -0,0 +1,33 @@
+# Calculate gallons of paint needed to paint the ceiling of a room.
+# Prompt for the length and width, and assume one gallon covers 350 square feet.
+# Display the number of gallons needed to paint the ceiling as a whole number.
+#
+# Example Output
+#        You will need to purchase 2 gallons of
+#        paint to cover 360 square feet.
+# Remember, you can’t buy a partial gallon of paint. You must round up to the next whole gallon.
+# Constraints
+# • Use a constant to hold the conversion rate.
+# • Ensure that you round up to the next whole number.
+
+#pseudocode
+#prompt for the length
+#store the length
+
+#prompt for the width
+#store the width
+
+#check is a number not: a string of characters/letters, a 0 in a string
+#boolean
+
+#calculate area of ceiling
+#length * width
+
+#calculate area in feet
+
+#calculate gallons needed
+#area in feet / 360
+#result * 2
+
+#displays gallons of paint needed(number_of_gallons, area_square_feet)
+#puts "You will need to purchase #{number_of_gallons} gallons of paint to cover #{area_square_feet)} square feet."
