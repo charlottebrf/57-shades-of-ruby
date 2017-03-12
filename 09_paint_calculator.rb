7d61c6f22269c6da63bca7965ecd0708f5b0748f
Charlotte Fereday <cbrfereday@gmail.com>
2017-03-12 21:39:48 +0000

    Updates the paint calculator with the main programme, and finishes exercise

--- 09_paint_calculator.rb
+++ 09_paint_calculator.rb
@@ -32,6 +32,7 @@
 #displays gallons of paint needed(number_of_gallons, area_square_feet)
 #puts "You will need to purchase #{number_of_gallons} gallons of paint to cover #{area_square_feet)} square feet."
 
+
 def gets_length
   puts "Please input the length in feet"
   length = gets.chomp
@@ -42,12 +43,28 @@ def gets_width
   length = gets.chomp
 end
 
-#is there any way I can check if the input is definitely in feet
-
 def main
   length = gets_length()
   width = gets_width()
   main_programme(length, width)
+end
+
+def main_programme(length, width)
+  gets_length
+  gets_width
+
+  if are_valid?(length, width) == false
+    puts "The length or width you entered was incorrect. Please enter them again, ensuring they are whole numbers"
+    return
+  end
+
+  converted_length = convert_to_number(length)
+  converted_width = convert_to_number(width)
+
+  area = calculate_area_of_ceiling(converted_length, converted_width)
+  gallons = calculate_gallons(area)
+
+  displays_gallons(gallons, area)
 end
 
 def contains_digits?(str)
@@ -60,25 +77,45 @@ def contains_digits?(str)
   end
 end
 
-def convert_to_number(str)
-  str.to_i
+def are_valid?(length, width)
+  if !contains_digits?(length) || !contains_digits?(width)
+    false
+  else
+    true
+  end
 end
 
-def not_valid_error_message #as part of the logic of the programme if contains_digits == false then... double check that this does not need to include an if statement
-  puts "Please input your length & width again as digits. For example, 5, 6."
+def convert_to_number(str)
+  str.to_i
 end
 
 def calculate_area_of_ceiling(length, width)
   length * width
 end
 
-#for paint calculator math separated into separate file 
+Gallons = 350
+def calculate_gallons(area)
+  gallons, remainder = area.divmod(Gallons)
+  if remainder > 0
+    gallons + 1
+  else
+    gallons
+  end
+end
 
-# def displays_gallons(calculate_gallons, calculate_area_of_ceiling)
-#   puts "You will need to purchase #{calculate_gallons} gallons of paint to cover #{area_square_feet)} square feet."
-# end
+def displays_gallons(gallons, area)
+  if gallons == 1
+      puts "You will need to purchase #{gallons} gallon of paint to cover #{area} square feet."
+    else
+      puts "You will need to purchase #{gallons} gallons of paint to cover #{area} square feet."
+    end
+end
+
 
 RSpec.describe "creates a paint calculator" do
+  it "runs the main programme" do
+    expect{ main_programme("5", "20") }.to output(/purchase 1 gallon of paint to cover 100 /).to_stdout
+  end
   it "returns true if the string contains a digit" do
     expect(contains_digits?("123")).to eq true
   end
@@ -91,31 +128,34 @@ RSpec.describe "creates a paint calculator" do
   it "converts a string to a number" do
     expect(convert_to_number("12")).to eq (12)
   end
-  it "Prints an error message" do
-    expect{ not_valid_error_message}.to output(/Please input your length & width again as digits/).to_stdout
+  it "returns true if the input is valid" do
+    expect(are_valid?("5","3")).to eq (true)
+  end
+  it "returns false if the input is invalid" do
+    expect(are_valid?("t@£","68d")).to eq (false)
   end
   it "calculates the area of the ceiling" do
     expect(calculate_area_of_ceiling(5,5)).to eq (25)
   end
-  # it "returns 1 if the area is less than or equal to 350" do
-  #   expect(calculate_gallons(280)).to eq (1)
-  # end
-  # it "returns 2 if the area is less than or equal to 700" do
-  #   expect(calculate_gallons(600)).to eq (2)
-  #   expect(calculate_gallons(360)).to eq (2)
-  # end
-  # it "returns 3 if the area is less than or equal to 1050" do
-  #   expect(calculate_gallons(900)).to eq (3)
-  # end
-  # it "returns 2 if the area if more than 350 and less than 700" do
-  #   expect(calculate_gallons(400)).to eq (2)
-  # end
+  it "returns 1 if the area is less than or equal to 350" do
+    expect(calculate_gallons(280)).to eq (1)
+    expect(calculate_gallons(350)).to eq (1)
+  end
+  it "returns 2 if the area is less than or equal to 700" do
+    expect(calculate_gallons(600)).to eq (2)
+    expect(calculate_gallons(360)).to eq (2)
+  end
+  it "returns 3 if the area is less than or equal to 1050" do
+    expect(calculate_gallons(900)).to eq (3)
+  end
+  it "returns 2 if the area if more than 350 and less than 700" do
+    expect(calculate_gallons(400)).to eq (2)
+  end
   it "gives the number of gallons of paint needed according to the area" do
     expect(calculate_gallons(400)).to eq (2)
   end
-
+  it "displays the number of gallons needed for an area" do
+    expect{ displays_gallons(2, 750) }.to output(/purchase 2 gallons of paint to cover 750 /).to_stdout
+    expect{ displays_gallons(1, 280) }.to output(/purchase 1 gallon of paint to cover 280 /).to_stdout
+  end
 end
-
-  # it "displays the number of gallons needed for the area of the paint party" do
-  #   expect{ displays_gallons(1, 500)) }.to output(/1 gallons of paint to cover 500/).to_stdout
-  # end
