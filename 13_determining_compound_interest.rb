8e4f3beed0f47cf25855068db93d1e97ee750338
Charlotte Fereday <cbrfereday@gmail.com>
2017-03-25 11:23:52 +0000

    Completes exercise with final result

--- 13_determining_compound_interest.rb
+++ 13_determining_compound_interest.rb
@@ -28,45 +28,59 @@
 #
 # Display: $1500 invested at 4.3% for 6 years compounded 4 times per year is $1938.84
 
+#Look at how to avoid duplication here to stop repeating very similar looking methods
 
-class CompundedInterest
+def ask_for(question)
+  puts question
+  gets.chomp
+end
 
-  def initialize(principal_amount, rate_of_interest, number_of_years, compounded_interest)
+# What is the principal amount?
+# What is the rate?
+# What is the number of times the interest is compounded per year?
+# $1500 invested at 4.3% for 6 years compounded 4 times per year is $1938.84
+
+def main_program
+  principal_amount = ask_for("What is the principal amount?")
+  rate = ask_for("What is the rate?")
+  compounded = ask_for("What is the number of times the interest is compounded per year?")
+  years = ask_for("What is the number of years?")
+
+  interest_calculator = CompoundedInterest.new(principal_amount, rate, compounded, years)
+  compounded_interest = interest_calculator.calculates_compounded_interest
+  interest_calculator.displays_compounded_interest(compounded_interest)
+end
+
+
+
+class CompoundedInterest
+
+  def initialize(principal_amount, rate_of_interest, number_of_years, compounded)
     @principal_amount = principal_amount
     @rate_of_interest = rate_of_interest
     @number_of_years = number_of_years
-    @compounded = compounded_interest
+    @compounded = compounded
   end
 
   def calculates_compounded_interest
-    compounded_interest = @principal_amount.to_f * (( 1 + (@rate_of_interest.to_f / @compounded.to_f))* @compounded * @number_of_years)
+    compounded_interest = @principal_amount.to_f * ( 1 + (@rate_of_interest.to_f / 100) / @compounded.to_f) ** (@compounded.to_f * @number_of_years.to_f)
+    compounded_interest.round(2)
   end
 
   def displays_compounded_interest(compounded_interest)
-    puts "#{@principal_amount}invested at #{@rate_of_interest} for #{@number_of_years} years compounded #{@compounded} times per year is $#{compounded_interest}."
+    puts "#{@principal_amount} invested at #{@rate_of_interest} for #{@number_of_years} years compounded #{@compounded} times per year is $#{compounded_interest}."
   end
-#Do some tests to see if the math is functioning as expected & i've interpretted the equation as should have
-
 
 end
 
-#Look at how to avoid duplication here to stop repeating very similar looking methods
-def gets_principal_amount
-  puts "What is the principal amount?"
-  principal_amount = gets.chomp
-end
-
-def gets_rate_of_interest
-  puts "What is the rate?"
-  rate_of_interest = gets.chomp
-end
-
-def gets_number_of_years
-  puts "What is the number of years?"
-  number_of_years = gets.chomp
-end
-
-def gets_compounded
-  puts "What is the number of times the interest is compounded per year?"
-  compounded = gets.chomp
-end
+main_program
+# RSpec.describe "calculates compound interest" do
+#   it "calculates the interest" do
+#     amount1 = CompoundedInterest.new(1500, 4.3, 6, 4)
+#     expect(amount1.calculates_compounded_interest).to eq (1938.84)
+#   end
+#   it "displays the amount of compounded interest" do
+#     amount1 = CompoundedInterest.new(1500, 4.3, 6, 4)
+#     expect { amount1.displays_compounded_interest(1938.84)}.to output( /1500 invested at 4.3 for 6 years compounded 4 times per year is \$1938.84./ ).to_stdout
+#   end
+# end
