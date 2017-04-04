7a7f0c0aa0c6b3f12189bddba5a2c0829c01f21a
Charlotte Fereday <cbrfereday@gmail.com>
2017-04-04 18:44:51 +0100

    Updates final exercise with solution

--- 18_temperature_converter.rb
+++ 18_temperature_converter.rb
@@ -20,10 +20,13 @@
 #
 
 class Converter
-
-  def initalize(farenheit, celsius)
-    @farenheit = farehenit
+  def initialize(farenheit, celsius)
+    @farenheit = farenheit
     @celsius = celsius
+  end
+
+  def converts_string(string)
+    string.to_f
   end
 
   def celsius_converter
@@ -31,11 +34,67 @@ class Converter
   end
 
   def farenheit_converter
-    (C * 9/5) + 32
+    (@celsius * 9/5) + 32
   end
-
 end
 
-def converts_string(string)
-  string.to_f
+def asks_temperature(string)
+  puts "Please enter the temperature in #{string}"
+  gets.chomp
+end
+
+
+def displays_message
+  puts "Press C to convert from Farenheit to Celsius. Press F to convert from Celsius to Farenheit."
+  gets.chomp
+end
+
+def displays_choice(choice)
+  if choice == "C" || choice == "c"
+    "Your choice: C"
+  elsif choice == "F" || choice == "f"
+    "Your choice: F"
+  else
+    puts "I'm sorry I didn't recognise your response. Please input again with either 'C' or 'F'."
+  end
+end
+
+def displays_temperature(choice, converted_tempertaure)
+  puts "The temperature in #{choice} is #{converted_tempertaure}."
+end
+
+def main_program
+  farenheit = asks_temperature("farenheit")
+  celsius = asks_temperature("celsius")
+  displays_message
+  displays_choice
+end
+
+
+
+# Press C to convert from Farenheit to Celsius.
+# Press F to convert from Celsius to Farenheit.
+# Your choice: C
+
+
+RSpec.describe "converts an amount from farenheit to celsius & vice versa" do
+  it "given an amount in farenheit converts the given amount to celsius" do
+    input1 = Converter.new(32, 20)
+    expect(input1.celsius_converter).to eq 0
+  end
+  it "given an amount in celsius converts the given amount to farenheit" do
+    input1 = Converter.new(32, 20)
+    expect(input1.farenheit_converter).to eq 68
+  end
+  it "given C or c will print C" do
+    expect(displays_choice("C")).to eq "Your choice: C"
+    expect(displays_choice("c")).to eq "Your choice: C"
+  end
+  it "given F will print F" do
+    expect(displays_choice("F")).to eq "Your choice: F"
+    expect(displays_choice("f")).to eq "Your choice: F"
+  end
+  # it "if input is anything other than C or F then user should receive an error message" do
+  #   expect(choice("c")).to eq "I'm sorry I didn't recognise your response. Please input again with either 'C' or 'F'."
+  # end
 end
