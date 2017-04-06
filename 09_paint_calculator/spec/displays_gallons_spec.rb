745869f6034ed12c5f23fdfde5880e5da97f3731
Charlotte Fereday <cbrfereday@gmail.com>
2017-04-06 11:15:00 +0100

    Updates tests for displays gallons spec

---
+++ 09_paint_calculator/spec/displays_gallons_spec.rb
@@ -0,0 +1,13 @@
+require_relative "../displays_gallons"
+
+describe "displays gallons" do
+  it "displays a gallons confirmation message in singular for one gallon of paint" do
+    displayer = DisplaysGallons.new(1, 330)
+    expect{ displayer.displays_gallons() }.to output(/purchase 1 gallon of paint to cover 330/).to_stdout
+  end
+  it "displays a gallons confirmation message in plural for more than one gallon of paint" do
+    displayer = DisplaysGallons.new(2, 750)
+    expect{ displayer.displays_gallons() }.to output(/purchase 2 gallons of paint to cover 750/).to_stdout
+  end
+
+end
