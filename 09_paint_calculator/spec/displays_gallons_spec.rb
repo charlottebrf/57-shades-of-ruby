2f073e1ec84e6d625b05da4e0069574897e6fe1e
Charlotte Fereday <cbrfereday@gmail.com>
2017-04-06 15:25:13 +0100

    Updates with the correct method name

--- 09_paint_calculator/spec/displays_gallons_spec.rb
+++ 09_paint_calculator/spec/displays_gallons_spec.rb
@@ -3,11 +3,11 @@ require_relative "../displays_gallons"
 describe "displays gallons" do
   it "displays a gallons confirmation message in singular for one gallon of paint" do
     displayer = DisplaysGallons.new(1, 330)
-    expect{ displayer.displays_gallons() }.to output(/purchase 1 gallon of paint to cover 330/).to_stdout
+    expect{ displayer.show_message() }.to output(/purchase 1 gallon of paint to cover 330/).to_stdout
   end
   it "displays a gallons confirmation message in plural for more than one gallon of paint" do
     displayer = DisplaysGallons.new(2, 750)
-    expect{ displayer.displays_gallons() }.to output(/purchase 2 gallons of paint to cover 750/).to_stdout
+    expect{ displayer.show_message() }.to output(/purchase 2 gallons of paint to cover 750/).to_stdout
   end
 
 end
