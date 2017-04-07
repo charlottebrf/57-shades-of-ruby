ffdb4cd3af40e128cda7681d02f57dd686fb8719
Charlotte Fereday <cbrfereday@gmail.com>
2017-04-07 10:11:19 +0100

    Updates the tests for display methods

--- 08_pizza_party/display.rb
+++ 08_pizza_party/display.rb
@@ -1,7 +1,6 @@
 
 class Display
 
-#need to add in the test for this method
   def asks_question(people_or_pizzas)
     puts "How many #{people_or_pizzas} are there?"
   end
@@ -10,17 +9,16 @@ class Display
     puts "#{people} people with #{pizzas} pizzas"
   end
 
-  def slices_per_person(slices)
-    puts "Each person gets #{slices} pieces of pizza."
+  def slices(num_slices)
+    puts "Each person gets #{num_slices} pieces of pizza."
   end
 
-  def left_over_slices(num)
-    puts "There are #{num} leftover pieces."
+  def left_over(num_leftover)
+    puts "There are #{num_leftover} leftover pieces."
   end
 
-#set up a test for this 
   def error_message
-    "Sorry we couldn't process your input. Please enter a whole number so we can calculate the pizza party numbers!"
+    puts "Sorry we couldn't process your input. Please enter a whole number so we can calculate the pizza party numbers!"
   end
 
 end
--- 08_pizza_party/spec/display_spec.rb
+++ 08_pizza_party/spec/display_spec.rb
@@ -1,13 +1,30 @@
-# require_relative "../display"
-#
-# describe "the display" do
-#   it "" do
-#     calculator = Calculator.new(10, 5)
-#     expect(calculator.area()).to eq (50)
-#   end
-#   it "calculates the quantity of gallons" do
-#     calculator = Calculator.new(10, 5)
-#     expect(calculator.calculate_gallons()).to eq (1)
-#   end
-# expect {display_area_in_metres(10)}.to output(/10 square metres/).to_stdout
-# # end
+require_relative "../display"
+#do I need to require keyboard? Or not because it's keyboard with the instance variable that requires the display?
+
+describe "the display" do
+  it "asks how many people or people there are" do
+    display = Display.new
+    expect { display.asks_question("people") }.to output(/How many people are there?/).to_stdout
+    expect { display.asks_question("pizzas") }.to output(/How many pizzas are there?/).to_stdout
+  end
+  it "confirms the users' input" do
+    display = Display.new
+    expect { display.confirms_input("12", "10") }.to output(/12 people with 10 pizzas/).to_stdout
+  end
+  it "confirms the users' input" do
+    display = Display.new
+    expect { display.confirms_input("12", "10") }.to output(/12 people with 10 pizzas/).to_stdout
+  end
+  it "confirms the number of slices per person" do
+    display = Display.new
+    expect { display.slices("5") }.to output(/gets 5 pieces of pizza/).to_stdout
+  end
+  it "confirms the number of leftover slices" do
+    display = Display.new
+    expect { display.left_over("7") }.to output(/7 leftover pieces/).to_stdout
+  end
+  it "displays an error message" do
+    display = Display.new
+    expect { display.error_message() }.to output(/Sorry we couldn't process your input./).to_stdout
+  end
+end
