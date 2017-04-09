34fbc27b395a707c91fe1a90b4ebe7715d918036
Charlotte Fereday <cbrfereday@gmail.com>
2017-04-09 21:29:48 +0100

    Refactors the methods from calculator class & updates the tests

--- 08_pizza_party/calculator.rb
+++ 08_pizza_party/calculator.rb
@@ -1,27 +1,28 @@
 
 class Calculator
+  def initialize(people, pizzas)
+    @people = people
+    @pizzas = pizzas
+  end
+
 
   SLICES = 8
 
-  def calculates_total_pizza_slices(pizzas)
-    total_slices = pizzas * SLICES
-    total_slices
+  def total
+    @pizzas * SLICES
   end
 
-  #error message if there's 0
-  def calculates_number_of_pizza_slices_per_person(people, pizzas)
-    slice_per_person = calculates_total_pizza_slices(pizzas) / people
-    slice_per_person = slice_per_person.round
-    if slice_per_person.even?
-      slice_per_person
+  def slices
+    slices = (total() / @people).round
+    if slices.even?
+      slices
     else
-      slice_per_person - 1
+      slices - 1
     end
   end
 
-  def calculates_number_of_left_over_slices(people, pizzas)
-    remainder = calculates_number_of_pizza_slices_per_person(people, pizzas) * people
-    calculates_total_pizza_slices(pizzas) - remainder
+  def left_slices
+    remainder = slices() * @people
+    total() - remainder
   end
-
 end
--- 08_pizza_party/spec/calculator_spec.rb
+++ 08_pizza_party/spec/calculator_spec.rb
@@ -1,12 +1,16 @@
-# require_relative "../calculator"
+require_relative "../calculator"
 
-# describe "the calculator" do
-#   it "" do
-#     calculator = Calculator.new(10, 5)
-#     expect(calculator.area()).to eq (50)
-#   end
-#   it "calculates the quantity of gallons" do
-#     calculator = Calculator.new(10, 5)
-#     expect(calculator.calculate_gallons()).to eq (1)
-#   end
-# end
+describe "the calculator" do
+  it "calculates the total number of slices" do
+    calculator = Calculator.new(10, 5)
+    expect(calculator.total()).to eq (40)
+  end
+  it "calculates the number of slices per person" do
+    calculator = Calculator.new(10, 5)
+    expect(calculator.slices()).to eq (4)
+  end
+  it "calculates the number of left over slices" do
+    calculator = Calculator.new(10, 5)
+    expect(calculator.left_slices()).to eq (0)
+  end
+end
